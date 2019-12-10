c42f6c9556e77ed18016891f7fe24891335f7a2c
Vladimir Burilko <Vladimir.Burilko@materialise.kiev.ua>
2019-12-10 18:43:10 +0200

    init

---
+++ Test.WorkflowCore.Core31.EF31/SomeWorkflow.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using WorkflowCore.Interface;
+using WorkflowCore.Models;
+
+namespace Test.WorkflowCore.Core31.EF31
+{
+    public class SomeWorkflow : IWorkflow
+    {
+        public void Build(IWorkflowBuilder<object> builder)
+        {
+            builder
+                .StartWith(context =>
+                {
+                    Console.WriteLine("Workflow started");
+                    return ExecutionResult.Next();
+                })
+                .Then(context =>
+                {
+                    Console.WriteLine("workflow complete");
+                    return ExecutionResult.Next();
+                });
+        }
+
+        public string Id { get; } = "SomeWorkflow";
+        public int Version { get; } = 1;
+    }
+}
---
+++ Test.WorkflowCore.Core31.EF31/Startup.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using WorkflowCore.Interface;
+
+namespace Test.WorkflowCore.Core31.EF31
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddWorkflow(o => o.UseSqlServer("", true, true));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            var workflowHost = app.ApplicationServices.GetRequiredService<IWorkflowHost>();
+
+            workflowHost.RegisterWorkflow<SomeWorkflow>();
+
+            workflowHost.Start();
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
